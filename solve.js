c62fe4d0e047d86645ca573b59b8a35cc16f25f0
Valio129 <88195264+Valio129@users.noreply.github.com>
2022-09-29 18:18:59 +0300

    Added new features
    Added restart function: It asks the player whether wants to play again or not.
    Added limited tries: the player has to try to guess the number in only few tries.

--- solve.js
+++ solve.js
@@ -1,31 +1,66 @@
 function guessANumber() {
-    const readline = require('readline').createInterface({
+    let tries = 10
+    win = false
+    let triesCounter = tries
+    console.log(`You have ${triesCounter} attepts to beat the game.`);
+    var readline = require('readline').createInterface({
         input: process.stdin,
         output: process.stdout
     })
     let computerGuess = Math.floor(Math.random() * 180)
     let guess
     let recursiveAsyncReadLine = function () {
-        readline.question('Guess the number (0-100): ', number => {
-            guess = Number(nunober)
-            if (guess <= 100 && guess >= 0) {
-                if (guess === computerGuess) {
-                    console.log('You guess it!')
-                    return readline.close()
-                } else if (guess < computerGuess) {
-                    console.log('Too Low!');
-                    recursiveAsyncReadLine()
-                } else if (guess > computerGuess) {
-                    console.log('Too High!');
-                    recursiveAsyncReadLine()
-                }
-
-            } else {
-                console.log('Invalid input! Try again...');
-                recursiveAsyncReadLine()
+        if (triesCounter === 0) {
+            if (win !== true) {
+                console.log('Sorry, you lost :(');
             }
-        })
+            readline.question('Play again? (y/n):', answer => {
+
+                if (answer === 'y') {
+                    // guessANumber()
+                    console.log('Okay!');
+                    triesCounter = tries
+                    win = true
+                    console.log(`You have ${triesCounter} attepts to beat the game.`);
+                    recursiveAsyncReadLine()
+                } else {
+                    console.log('Okay, bye!');
+                    return readline.close()
+                }
+            })
+        } else {
+
+            readline.question('Guess the number (0-100): ', number => {
+                guess = Number(number)
+                if (guess <= 100 && guess >= 0) {
+                    if (guess === computerGuess) {
+                        console.log('You guess it!')
+                        win = true
+                        triesCounter = 0
+                        return
+
+                    } else if (guess < computerGuess) {
+                        console.log('Too Low!');
+                        triesCounter--
+                        console.log(`${triesCounter} attepts left`);
+                        recursiveAsyncReadLine()
+                    } else if (guess > computerGuess) {
+                        console.log('Too High!');
+                        triesCounter--
+                        console.log(`${triesCounter} attepts left`);
+                        recursiveAsyncReadLine()
+                    }
+
+                } else {
+                    console.log('Invalid input! Try again...');
+                    recursiveAsyncReadLine()
+
+                }
+            })
+        }
+
     }
     recursiveAsyncReadLine();
+
 }
 guessANumber()
